629d1d4149877fc559412b7ab05cc71a510c22c9
Rusvik <tobias.r.rusvik@gmail.com>
2023-10-11 15:54:07 +0200

    Added more logic to the code, but not finished with the CreateUser func!

--- client/application.jsx
+++ client/application.jsx
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+import {Link, Route, Routes} from "react-router-dom";
+import {FormEvent} from "react";
 
 
 export function Application() {
@@ -15,7 +17,50 @@ export function Application() {
             <input type={"password"}/>
             <br />
             <input type={"submit"}/>
-            <p id={"createUser"}>Create User</p>
+            <Link to={"/create"}>Create User</Link>
         </form>
     </>;
+}
+
+function CreateUser(){
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Create a new user</h2>
+            <div>
+                <label>Full Name:</label>
+                <br />
+                <input type={"text"} />
+                <br />
+                <br />
+                <label>Email:</label>
+                <br />
+                <input type={"text"} />
+                <br />
+                <br />
+                <label>New password:</label>
+                <br />
+                <input type={"password"} />
+                <br />
+                <br />
+                <label>Repeat password</label>
+                <br />
+                <input type={"password"} />
+                <br />
+                <button>Submit</button>
+
+            </div>
+        </form>
+    )
+}
+
+function ApplicationRoutes(){
+    <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/create"} element={<CreateUser />} />
+        <Route path={"/*"} element={<h1>Page Not Found</h1>} />
+    </Routes>
 }
