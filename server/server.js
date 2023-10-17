aef1119e21de334486ffdb6113697515c4e25ebe
Rusvik <tobias.r.rusvik@gmail.com>
2023-10-17 15:39:16 +0200

    added a userApi where we will handle the DB. Also updated serveral package.json files. The database should also be connected, and password stored in secret file.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 import express from "express";
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+dotenv.config();
 
 const app = express();
 app.use(express.static("../client/dist"))
+
+const url = process.env.MONGODB_URL;
+const client = new MongoClient(url);
+
+client.connect().then((connection) => {
+    const db = connection.db("sample_mflix");
+    createUserRouter(db);
+});
+
 app.listen(3000);
